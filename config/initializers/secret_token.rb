5795433ceedb5cda56a79f41470a4809733deed8
hardmarlindart <mardem_lincow@yahoo.com.br>
2014-12-29 18:08:44 -0200

    Desenvilvimento do sistema Photos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photo::Application.config.secret_key_base = 'a66da60694d9c45a002daa29b306eec7589988837dc34a481f2a1b92ecc031022db2967b59e0551159d6baf0f02907d0779d8c6b92eebe2f25dd2fad3f6d0ff3'
